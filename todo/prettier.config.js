be1eb64a810368c8081dcfb3f42e1990e29d68c7
husanu <6e745v@egitr.com>
2022-03-03 08:27:50 +0200

    todo items

---
+++ todo/prettier.config.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 100,
+  parser: 'typescript',
+  semi: true,
+
+  "overrides": [
+    {
+      "files": "*.js",
+      "options": { "parser": "babylon" }
+    }
+  ]
+};
